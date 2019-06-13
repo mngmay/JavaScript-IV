bb5c18bda2b5a9056cb07a8675559beb8264f96b
May Ng <mngmay@gmail.com>
2019-06-13 13:14:52 -0700

    Completed MVP - prototype-refactor.js and lambda-class.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,199 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(newPerson){
+        this.name = newPerson.name;
+        this.age = newPerson.age;
+        this.location = newPerson.location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(newProf){
+        super(newProf);
+        this.specialty = newProf.speciality;
+        this.favLanguage = newProf.favLanguage;
+        this.catchPhrase = newProf.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person{
+    constructor(newStudent){
+        super(newStudent);
+        this.previousBackground = newStudent.previousBackground;
+        this.className = newStudent.className;
+        this.favSubjects = newStudent.favSubjects;
+    }
+
+    listsSubjects() {
+        return this.favSubjects.forEach(subject => console.log(subject));
+    }
+
+
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}`
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(pM){
+        super(pM);
+        this.gradClassName = pM.gradClassName;
+        this.favInstructor = pM.favInstructor;
+    }
+
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 'Infinity',
+    location: 'Denver',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!',
+});
+
+  const isaiah = new Student({
+    name: 'Isaiah',
+    age: 18,
+    location: 'Florida',
+    previousBackground: 'High School last month',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+    const kevin = new Student({
+    name: "Kevin",
+    age: 28,
+    location: "California",
+    previousBackground: "Table Games Dealer",
+    className: "WEB21",
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+    const nisa = new Student({
+    name: 'Nisa',
+    age: 25,
+    location: 'Ohio',
+    previousBackground: 'Debt Collector',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+
+    const joscelyn = new Student({
+    name: "Joscelyn",
+    age: 29,
+    location: "California",
+    previousBackground: "English teacher",
+    className: 'Web21',
+    favSubjects: ["Computer Science", "Philosophy", "English"],
+});
+
+  const marguel = new ProjectManager({
+    name: 'Marguel',
+    age: 'Maybe 26',
+    gradClassName: 'WEBPT2',
+    favInstructor: 'Me?',
+    location: 'California',
+    specialty: 'React',
+    favLanguage: 'JavaScript, Python, Elm etc.',
+    catchPhrase: "Practice Flex Zombies !!!",
+});
+
+  const brandon = new ProjectManager({
+    name: 'Brandon',
+    age: '34',
+    gradClassName: 'WEB18',
+    favInstructor: 'Professor Lambda',
+    location: 'Maine',
+    specialty: 'Redux',
+    favLanguage: 'JavaScript, C++, Python.',
+    catchPhrase: "You shall not pass!",
+});
+
+    const mary = new ProjectManager({
+    name: 'Mary',
+    age: '24',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: "That looks AWESOME",
+});
+
+    const christian = new ProjectManager({
+    name: 'Christian',
+    age: '32',
+    gradClassName: 'WEB18',
+    favInstructor: 'Every Instructor in Lambda',
+    location: 'Seattle, WA',
+    specialty: 'Data Structures & Algorithms',
+    favLanguage: 'JavaScript',
+    catchPhrase: "Dont forget your daily commit.",
+});
+
+    const pat = new ProjectManager({
+    name: 'Pat',
+    age: '38',
+    gradClassName: 'WEB18',
+    favInstructor: 'Brett Madrid',
+    location: 'Petaluma, Ca',
+    specialty: 'Empathetic to the struggle of Redux',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Lets google that together.'
+});
+
+    const darren = new ProjectManager({
+    name: 'Darren',
+    age: '25',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'North Carolina',
+    specialty: 'React',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Gang. Gang.',
+});
+
+    const austin = new ProjectManager({
+    name: 'Austin',
+    age: '23',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'Somewhere',
+    specialty: 'Java',
+    favLanguage: 'Java',
+    catchPhrase: ":eggplant:",
+});
+
+
+console.log(dan.speak());
+console.log(dan.grade(joscelyn, 'JavaScript'));
+console.log(dan.demo('Cats'));
+kevin.listsSubjects();
+joscelyn.listsSubjects();
+console.log(nisa.PRAssignment('JavaScript'));
+console.log(kevin.sprintChallenge('JavaScript'));
+console.log(mary.standUp('Web21'));
+console.log(brandon.debugsCode(nisa, 'JavaScript'));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,356 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+/*
+  === GameObject ===
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+*/
+
+class GameObject {
+  constructor(newObject) {
+    this.createdAt = newObject.createdAt;
+    this.name = newObject.name;
+    this.dimensions = newObject.dimensions;
+  }
+
+  destroy() {
+    this.healthPoints = 0;
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+/*
+    === CharacterStats ===
+    * healthPoints
+    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+    * should inherit destroy() from GameObject's prototype
+  */
+
+class CharacterStats extends GameObject {
+  constructor(stats) {
+    super(stats);
+    this.healthPoints = stats.healthPoints; //special attribute to CharacterStats
+  }
+
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+/*
+    === Humanoid (Having an appearance or character resembling that of a human.) ===
+    * team
+    * weapons
+    * language
+    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+    * should inherit destroy() from GameObject through CharacterStats
+    * should inherit takeDamage() from CharacterStats
+  */
+
+class Humanoid extends CharacterStats {
+  constructor(char) {
+    super(char);
+    this.team = char.team;
+    this.weapons = char.weapons;
+    this.language = char.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
+
+/*
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+class Villain extends Humanoid {
+  constructor(villain){
+      super(villain); //binds to Humanoid for inheritence
+    this.evillaugh = villain.evillaugh;
+    this.minion = villain.minion;
+}
+
+throwShade(hero) {
+    let dmg = Math.floor(Math.random() * 5);
+    if (hero.healthPoints <= 0) {
+      return `${
+        hero.name
+      } is already dead and can't get their feelings hurt anymore.`;
+    } else if (dmg === 0) {
+      return `No good, try again. 0 damage.`;
+    } else if (hero.healthPoints <= dmg) {
+      hero.healthPoints -= dmg;
+      return `${this.name} threw shade at ${
+        hero.name
+      } for ${dmg}HP damage and knocked them out!`;
+    }
+    hero.healthPoints -= dmg;
+    return `${this.name} threw shade at ${
+      hero.name
+    } and hurt their feelings ${dmg}HP damage. That wasn't very nice.`;
+  };
+
+  apologize(hero) {
+    let heal = Math.floor(Math.random() * 5);
+    if (this.healthPoints <= 0) {
+      return `${
+        this.name
+      } attempted to say sorry but is already done for. It's too late to 'pologize. Nice try.`;
+    }
+    this.healthPoints += heal;
+    return `${this.name} apologized to ${hero.name} and gained ${heal}HP. Sorry!`;
+  };
+};
+
+
+
+// //Villain Attacks - commented out for refactor
+
+// Villain.prototype.throwShade = function(hero) {
+//   let dmg = Math.floor(Math.random() * 5);
+//   if (hero.healthPoints <= 0) {
+//     return `${
+//       hero.name
+//     } is already dead and can't get their feelings hurt anymore.`;
+//   } else if (dmg === 0) {
+//     return `No good, try again. 0 damage.`;
+//   } else if (hero.healthPoints <= dmg) {
+//     hero.healthPoints -= dmg;
+//     return `${this.name} threw shade at ${
+//       hero.name
+//     } for ${dmg}HP damage and knocked them out!`;
+//   }
+//   hero.healthPoints -= dmg;
+//   return `${this.name} threw shade at ${
+//     hero.name
+//   } and hurt their feelings ${dmg}HP damage. That wasn't very nice.`;
+// };
+
+// Villain.prototype.apologize = function(hero) {
+//   let heal = Math.floor(Math.random() * 5);
+//   if (this.healthPoints <= 0) {
+//     return `${
+//       this.name
+//     } attempted to say sorry but is already done for. It's too late to 'pologize. Nice try.`;
+//   }
+//   this.healthPoints += heal;
+//   return `${this.name} apologized to ${hero.name} and gained ${heal}HP. Sorry!`;
+// };
+
+//logic in progress
+
+//generate poison dmg
+//add poisoned method to target object. figure out how to invoke method each time target takes action
+//remove property in another heal method
+
+// Villain.prototype.poisonAttack = function(hero) {
+//   let dmg = Math.floor(Math.random() * 5);
+//   if (hero.healthPoints <= 0) {
+//     return `${hero.name} is already dead and can't be poisoned.`;
+//   }
+//   hero.healthPoints -= dmg;
+//   return `${
+//     this.name
+//   } poisoned ${hero}. Damage is inflicted for each of ${hero}'s turn until removed. ${dmg}HP damage this turn.`;
+// };
+
+// <----- End Villain Attacks
+
+class Hero extends Humanoid {
+    constructor(hero){
+        super(hero);
+        this.motto = hero.motto;
+        this.costume = hero.costume;
+    }
+
+    giveCompliment(villain) {
+        let heal = Math.floor(Math.random() * 8);
+        if (this.healthPoints <= 0) {
+          return `${
+            this.name
+          } is already done for and can't give compliments. Nice try.`;
+        }
+        this.healthPoints += heal;
+        return `${this.name} gave ${
+          villain.name
+        } a compliment and gained ${heal}HP for a total of ${this.healthPoints} HP!`;
+      };
+
+      guiltTrip(villain) {
+        let dmg = Math.floor(Math.random() * 5);
+        if (villain.healthPoints <= 0) {
+          return `${villain.name} is already dead and can't feel guilty.`;
+        } else if (dmg === 0) {
+          return `That was a bad guilt trip, they don't feel guilty at all. 0 damage.`;
+        } else if (villain.healthPoints <= dmg) {
+          villain.healthPoints -= dmg;
+          return `${this.name} guilt tripped ${
+            villain.name
+          } for ${dmg}HP damage and knocked them out!`;
+        }
+        villain.healthPoints -= dmg;
+        return `${this.name} guilt tripped ${
+          villain.name
+        } and now they feel bad. ${dmg}HP damage.`;
+      };
+}
+
+
+//Hero Attacks  -- commented out for refactor
+
+// Hero.prototype.giveCompliment = function(villain) {
+//   let heal = Math.floor(Math.random() * 8);
+//   if (this.healthPoints <= 0) {
+//     return `${
+//       this.name
+//     } is already done for and can't give compliments. Nice try.`;
+//   }
+//   this.healthPoints += heal;
+//   return `${this.name} gave ${
+//     villain.name
+//   } a compliment and gained ${heal}HP for a total of ${this.healthPoints} HP!`;
+// };
+
+// Hero.prototype.guiltTrip = function(villain) {
+//   let dmg = Math.floor(Math.random() * 5);
+//   if (villain.healthPoints <= 0) {
+//     return `${villain.name} is already dead and can't feel guilty.`;
+//   } else if (dmg === 0) {
+//     return `That was a bad guilt trip, they don't feel guilty at all. 0 damage.`;
+//   } else if (villain.healthPoints <= dmg) {
+//     villain.healthPoints -= dmg;
+//     return `${this.name} guilt tripped ${
+//       villain.name
+//     } for ${dmg}HP damage and knocked them out!`;
+//   }
+//   villain.healthPoints -= dmg;
+//   return `${this.name} guilt tripped ${
+//     villain.name
+//   } and now they feel bad. ${dmg}HP damage.`;
+// };
+
+// <----- End Hero Attacks
+
+// Villains
+
+const drDrama = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 3,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Dr. Drama",
+  team: "The Bad Guys",
+  weapons: ["Social Media"],
+  language: "Gossip Speak",
+  evillaugh: "Muwhahaha",
+  minion: "Yippy Dog"
+});
+
+// Heroes
+
+const niceGuy = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 5,
+    width: 1,
+    height: 3
+  },
+  healthPoints: 8,
+  name: "Captain Nice Guy",
+  team: "The Good Guys",
+  weapons: ["Charitable Acts"],
+  language: "Honesty",
+  motto: "You Betcha!",
+  costume: "Spandex and Cape"
+});
+
+//Fight between Good VS Evil
+
+console.log(`Captain Nice Guy says, "${niceGuy.motto}"`);
+console.log(
+  `Captain Nice Guy's greatest method of attack is "${niceGuy.weapons}"`
+);
+console.log(
+  `Captain Nice Guy prefers to wear a costume of a "${niceGuy.costume}"`
+);
+console.log(`Dr. Drama let out a big "${drDrama.evillaugh}"`);
+console.log(`Dr. Drama's trusty sidekick is a ${drDrama.minion}`);
+console.log(
+  `Dr. Drama likes to use ${drDrama.weapons} in his method of attack`
+);
+console.log(drDrama.throwShade(niceGuy));
+console.log(niceGuy.giveCompliment(drDrama));
+console.log(niceGuy.guiltTrip(drDrama));
+console.log(niceGuy.guiltTrip(drDrama));
+console.log(niceGuy.guiltTrip(drDrama));
+console.log(niceGuy.guiltTrip(drDrama));
+console.log(niceGuy.guiltTrip(drDrama));
+console.log(drDrama.apologize(niceGuy));
